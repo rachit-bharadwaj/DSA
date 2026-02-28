215ecbe0001ec95eb3978fe4b30f9b3f9b874fdd
Rachit Bharadwaj <rachitbharadwaj52@gmail.com>
2026-02-28 21:08:22 +0530

    feat: implement Valid Parentheses solution

---
+++ Stack/Valid Parentheses.cpp
@@ -0,0 +1,23 @@
+// Question: https://leetcode.com/problems/valid-parentheses/
+
+// Difficulty: Easy
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for (char c : s) {
+            if (c == '(' || c == '{' || c == '[') {
+                st.push(c);
+            } else {
+                if (st.empty() || !((st.top() == '(' && c == ')') ||
+                                    (st.top() == '{' && c == '}') ||
+                                    (st.top() == '[' && c == ']'))) {
+                    return false;
+                }
+                st.pop();
+            }
+        }
+        return st.empty();
+    }
+};
