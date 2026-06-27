38d1a1faf8e56506a7a4bd35e8f0fc2dc785f605
Rachit Bharadwaj <rachitbharadwaj52@gmail.com>
2026-06-27 07:27:44 +0530

    feat: add implementation for Divisible Sum Pairs problem

---
+++ Array/Divisible Sum Pairs.cpp
@@ -0,0 +1,22 @@
+// Question: https://www.hackerrank.com/challenges/divisible-sum-pairs/problem
+
+// Difficulty: Easy
+
+int divisibleSumPairs(int n, int k, vector<int> ar)
+{
+    int count = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (i < j)
+            {
+                if ((ar[i] + ar[j]) % k == 0)
+                    count++;
+            }
+        }
+    }
+
+    return count;
+}
