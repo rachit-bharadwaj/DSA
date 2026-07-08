b43344cb501f5a1265d47416c98b0bf458fd43c4
Rachit Bharadwaj <rachitbharadwaj52@gmail.com>
2026-07-08 09:59:37 +0530

    feat: add implementation for Tower Breakers problem

---
+++ Maths/Tower Breakers.cpp
@@ -0,0 +1,11 @@
+// Question: https://www.hackerrank.com/challenges/tower-breakers-1/problem
+
+// Difficulty: Easy
+
+int towerBreakers(int n, int m)
+{
+    if (m == 1 || n % 2 == 0)
+        return 2;
+
+    return 1;
+}
