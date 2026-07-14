baa2be8b2ddc9ccaa8f4308ee90778d9878f2134
Rachit Bharadwaj <rachitbharadwaj52@gmail.com>
2026-07-14 20:18:33 +0530

    feat: add implementation for Utopian Tree problem

---
+++ Maths/Utopian Tree.cpp
@@ -0,0 +1,22 @@
+// Question: https://www.hackerrank.com/challenges/utopian-tree/problem
+
+// Difficulty: Easy
+
+int utopianTree(int n)
+{
+    int height = 1;
+
+    for (int i = 1; i <= n; i++)
+    {
+        if (i % 2 == 1)
+        {
+            height *= 2;
+        }
+        else
+        {
+            height += 1;
+        }
+    }
+
+    return height;
+}
