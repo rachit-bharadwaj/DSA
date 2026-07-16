2d7b19443438b848c8d82328b077cd2a6cf8e42f
Rachit Bharadwaj <rachitbharadwaj52@gmail.com>
2026-07-17 05:07:02 +0530

    feat: add implementation for XOR Strings problem

---
+++ Strings/XOR Strings.cpp
@@ -0,0 +1,18 @@
+// Question: https://www.hackerrank.com/challenges/strings-xor/problem
+
+// Difficulty: Easy
+
+string strings_xor(string s, string t)
+{
+
+    string res = "";
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] == t[i])
+            res += '0';
+        else
+            res += '1';
+    }
+
+    return res;
+}
